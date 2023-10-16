8b5c9ce35bf8b5be1c98e99979e78da6fb470e33
Chris Flatley <chris@committed.io>
2023-10-16 22:21:05 +0100

    feat: add user media query

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  let connectButton = document.querySelector("#connect");
+  let output = document.querySelector("#output")
+
+  function addOutput(message) {
+    const li = document.createElement("li");
+    li.textContent = message
+    output.appendChild(li)
+  }
+
+  connectButton.addEventListener("click", (e) => {
+    addOutput("Start of connect click handler")
+    openMicrophone()
+    addOutput("End of connect click handler")
   });
+
+  function openMicrophone() {
+    navigator.mediaDevices.getUserMedia({
+      audio: true
+    }).then(
+      (mediaStream) => {
+        addOutput("GotUserMedia: media stream")
+        mediaStream.getTracks().forEach(x => x.stop())
+      }
+    ).catch((err) => addOutput(`Error getting user media: ${JSON.stringify(err)}`))
+  }
 });
